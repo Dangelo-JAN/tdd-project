393d53011336f7a978f87605e35abb9f11d66337
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-06-24 13:25:48 -0400

    Create fizzbuzz method

--- tdd_project.rb
+++ tdd_project.rb
@@ -5,4 +5,10 @@ class Solver
 
     num * factorial(num - 1)
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+  end
 end
